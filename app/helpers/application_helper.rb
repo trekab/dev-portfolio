69de7bfc06129bc0ca8c6d77f623b4682be1edd9
Treasure Kabareebe <treasurecalm@gmail.com>
2020-04-01 05:27:45 +0300

    Add new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
 	end
+
+  def copyright_generator
+    TrekabsViewTool::Renderer.copyright 'Treasure Kabareebe', 'All rights reserved'
+  end
 end
